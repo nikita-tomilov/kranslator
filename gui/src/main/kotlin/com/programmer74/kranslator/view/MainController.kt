369e300b056421da09b1a39894fbbf9c7c7ef454
Nikita Tomilov <programmer174@icloud.com>
2022-07-03 16:39:32 +0200

    gui: now result is imprinted onto image

--- gui/src/main/kotlin/com/programmer74/kranslator/view/MainController.kt
+++ gui/src/main/kotlin/com/programmer74/kranslator/view/MainController.kt
@@ -1,9 +1,6 @@
 package com.programmer74.kranslator.view
 
-import com.programmer74.kranslator.ocr.CharacterRecognizer
-import com.programmer74.kranslator.ocr.OCRLanguage
-import com.programmer74.kranslator.ocr.RIL_PARA
-import com.programmer74.kranslator.ocr.TextBlock
+import com.programmer74.kranslator.ocr.*
 import com.programmer74.kranslator.service.graphics.ImageUtils
 import com.programmer74.kranslator.service.ocr.RemoteTesseractKtor
 import com.programmer74.kranslator.service.translation.LibreTranslate
@@ -11,7 +8,8 @@ import com.programmer74.kranslator.translate.Translator
 import com.programmer74.kranslator.translate.TranslatorLanguage
 import com.programmer74.kranslator.translate.TranslatorRequest
 import javafx.application.Platform
-import javafx.embed.swing.SwingFXUtils
+import mu.KLogging
+import java.awt.*
 import java.awt.image.BufferedImage
 import java.nio.file.Files
 import java.util.concurrent.Executors
@@ -64,32 +62,113 @@ class MainController {
         callback(results.blocks)
       }
     }
-    //    ex.submit {
-    //      val tmp = Files.createTempFile("img", ".png").toFile()
-    //      ImageIO.write(originalImage.get(), "png", tmp)
-    //      val result = ocrInstance.recognize(tmp, 0, fromLanguage.get().toOCRLanguage(), RIL_PARA)
-    //      val adjustedImage = originalImage.get()
-    //      val g = originalImage.get().createGraphics()
-    //      g.color = Color.RED
-    //      result.blocks.forEach {
-    //        val block = it.block
-    //        g.drawRect(block.x, block.y, block.w, block.h)
-    //      }
-    //      val text = result.blocks.joinToString("\n") { it.text }
-    //      Platform.runLater {
-    //        originalText.set(text)
-    //        originalImageSwing.set(SwingFXUtils.toFXImage(adjustedImage, null))
-    //        actionInProgress.set(false)
-    //      }
-    //    }
   }
 
-  private fun TranslatorLanguage.toOCRLanguage(): OCRLanguage {
-    return when (this) {
-      TranslatorLanguage.EN_GB -> OCRLanguage.EN
-      TranslatorLanguage.EN_US -> OCRLanguage.EN
-      TranslatorLanguage.DE -> OCRLanguage.DE
-      TranslatorLanguage.RU -> OCRLanguage.RU
+  fun imprintTranslateResponseToImage(
+    image: BufferedImage,
+    ocrBlocks: List<TextBlock>,
+    translatedTexts: List<String>,
+    callback: (BufferedImage) -> Unit
+  ) {
+    ex.submit {
+      val target = image.deepCopy()
+      val g = target.createGraphics()
+      assert(ocrBlocks.size == translatedTexts.size)
+      (ocrBlocks.indices).forEach { i ->
+        val block = ocrBlocks[i].block
+        val text = translatedTexts[i]
+        renderTextBlock(text, block, g)
+      }
+      Platform.runLater {
+        callback(target)
+      }
     }
   }
+
+  private fun renderTextBlock(text: String, block: TextBlockRectangle, g: Graphics2D) {
+    g.color = Color.WHITE
+    g.fillRect(block.x, block.y, block.w, block.h)
+
+    val origStroke = g.stroke
+    g.color = Color.RED
+    g.stroke = BasicStroke(1.0f)
+    g.drawRect(block.x, block.y, block.w, block.h)
+
+    g.color = Color.BLACK
+    g.stroke = origStroke
+    drawTextToFit(text, g, block)
+  }
+
+  //https://stackoverflow.com/questions/12485236/finding-maximum-font-size-to-fit-in-region-java
+  private fun drawTextToFit(text: String, g: Graphics2D, maxRect: TextBlockRectangle) {
+    val lines = text.split("\n")
+    val linesCount = lines.count()
+
+    val lineHeight = maxRect.h / linesCount
+    val lineWidth = maxRect.w
+
+    val longestLine = lines.maxByOrNull { it.length } ?: lines.first()
+
+    g.font = Font("TimesRoman", Font.PLAIN, 16)
+    g.setRenderingHint(
+        RenderingHints.KEY_TEXT_ANTIALIASING,
+        RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+    )
+    logger.info { "drawing text block of size ${text.length} to ${maxRect.x};${maxRect.y}" }
+
+    val maxFontSize = getMaxFittingFontSize(g, g.font, longestLine, lineWidth, lineHeight)
+    logger.info { "drawing text of size ${text.length} to ${maxRect.x};${maxRect.y};${maxRect.w};${maxRect.h}; computed size: $maxFontSize" }
+
+    g.font = Font("TimesRoman", Font.PLAIN, maxFontSize)
+
+    val x = maxRect.x + 3
+    var y = maxRect.y - 3 + g.fontMetrics.height
+    lines.forEach { line ->
+      g.drawString(line, x, y)
+      y += g.fontMetrics.height
+    }
+  }
+
+  private fun getMaxFittingFontSize(
+    g: Graphics,
+    font: Font,
+    string: String?,
+    width: Int,
+    height: Int
+  ): Int {
+    var minSize = 6
+    var maxSize = 72
+    var curSize = font.size
+    while (maxSize - minSize > 2) {
+      val fm = g.getFontMetrics(Font(font.name, font.style, curSize))
+      val fontWidth = fm.stringWidth(string)
+      val fontHeight = fm.leading + fm.maxAscent + fm.maxDescent
+      if ((fontWidth > width) || (fontHeight > height)) {
+        maxSize = curSize
+        curSize = (maxSize + minSize) / 2
+      } else {
+        minSize = curSize
+        curSize = (minSize + maxSize) / 2
+      }
+    }
+    return curSize
+  }
+
+  companion object : KLogging()
+}
+
+private fun TranslatorLanguage.toOCRLanguage(): OCRLanguage {
+  return when (this) {
+    TranslatorLanguage.EN_GB -> OCRLanguage.EN
+    TranslatorLanguage.EN_US -> OCRLanguage.EN
+    TranslatorLanguage.DE -> OCRLanguage.DE
+    TranslatorLanguage.RU -> OCRLanguage.RU
+  }
+}
+
+private fun BufferedImage.deepCopy(): BufferedImage {
+  val cm = this.colorModel
+  val isAlphaPremultiplied = cm.isAlphaPremultiplied
+  val raster = this.copyData(this.raster.createCompatibleWritableRaster())
+  return BufferedImage(cm, raster, isAlphaPremultiplied, null)
 }
--- gui/src/main/kotlin/com/programmer74/kranslator/view/MainView.kt
+++ gui/src/main/kotlin/com/programmer74/kranslator/view/MainView.kt
@@ -1,24 +1,24 @@
 package com.programmer74.kranslator.view
 
+import com.programmer74.kranslator.ocr.TextBlock
 import com.programmer74.kranslator.translate.TranslatorLanguage
 import com.programmer74.kranslator.translate.TranslatorRequest
-import javafx.beans.property.SimpleBooleanProperty
-import javafx.beans.property.SimpleObjectProperty
-import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.*
 import javafx.embed.swing.SwingFXUtils
 import javafx.geometry.Orientation
 import javafx.scene.control.ProgressIndicator
+import javafx.scene.control.ScrollPane
 import javafx.scene.image.Image
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent.KEY_PRESSED
 import tornadofx.*
 import java.awt.image.BufferedImage
-import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.system.exitProcess
 
 class MainView : Fragment("Translator") {
 
+  private lateinit var rootHeight: ReadOnlyDoubleProperty
   private val controller = MainController()
   private val languages = controller.languages()
   private val fromLanguage = SimpleObjectProperty(TranslatorLanguage.DE)
@@ -26,12 +26,38 @@ class MainView : Fragment("Translator") {
   private val originalText = SimpleStringProperty("")
   private val originalImage = AtomicReference<BufferedImage>(null)
   private val originalImageSwing = SimpleObjectProperty<Image>()
-  private val imageOCRPerformed = AtomicBoolean(false)
+  private lateinit var originalImageViewPane: ScrollPane
+  private val imageOCRResults = AtomicReference<List<TextBlock>>(null)
   private val resultText = SimpleStringProperty("")
+  private val resultImageSwing = SimpleObjectProperty<Image>()
+  private lateinit var resultImageViewPane: ScrollPane
   private val actionInProgress = SimpleBooleanProperty(false)
 
   private fun translateAction() {
     actionInProgress.set(true)
+
+    if (imageOCRResults.get() != null) {
+      val requests =
+          imageOCRResults.get().map {
+            TranslatorRequest(
+                it.text,
+                fromLanguage.get(),
+                toLanguage.get())
+          }
+      controller.translate(requests) {
+        resultText.set(it.joinToString("\n"))
+        controller.imprintTranslateResponseToImage(
+            originalImage.get(),
+            imageOCRResults.get(),
+            it) { img ->
+          resultImageSwing.set(SwingFXUtils.toFXImage(img, null))
+          resultImageViewPane.prefHeightProperty().unbind()
+          resultImageViewPane.prefHeightProperty().bind(rootHeight)
+          actionInProgress.set(false)
+          imageOCRResults.set(null)
+        }
+      }
+    }
 
     if (originalText.get().isNotEmpty()) {
       val requests =
@@ -51,7 +77,7 @@ class MainView : Fragment("Translator") {
       actionInProgress.set(true)
       controller.ocr(originalImage.get(), fromLanguage.get()) { res ->
         originalText.set(res.joinToString("\n") { it.text })
-        imageOCRPerformed.set(true)
+        imageOCRResults.set(res)
         translateAction()
       }
       return
@@ -68,19 +94,29 @@ class MainView : Fragment("Translator") {
     controller.pasteImageFromClipboard({ clipboardImage ->
       originalImageSwing.set(SwingFXUtils.toFXImage(clipboardImage, null))
       originalImage.set(clipboardImage)
-      imageOCRPerformed.set(false)
+      originalImageViewPane.prefHeightProperty().unbind()
+      originalImageViewPane.prefHeightProperty().bind(rootHeight)
+      imageOCRResults.set(null)
       originalText.set("")
       actionInProgress.set(false)
     }, { error ->
       originalText.set(error)
       actionInProgress.set(false)
+      clearImageAction()
     })
+  }
+
+  private fun clearImageAction() {
+    originalImageViewPane.prefHeightProperty().unbind()
+    originalImageViewPane.prefHeightProperty().bind(SimpleDoubleProperty(0.0))
+    resultImageViewPane.prefHeightProperty().unbind()
+    resultImageViewPane.prefHeightProperty().bind(SimpleDoubleProperty(0.0))
   }
 
   override val root = form {
     prefWidth = 1280.0
     prefHeight = 720.0
-    val rootHeight = this.heightProperty()
+    rootHeight = this.heightProperty()
 
     hbox {
       label("From")
@@ -101,8 +137,8 @@ class MainView : Fragment("Translator") {
       }
     }
 
-    splitpane(orientation = Orientation.VERTICAL) {
-      splitpane {
+    splitpane {
+      vbox {
         textarea(originalText) {
           disableProperty().bind(actionInProgress)
           addEventHandler(KEY_PRESSED) { event ->
@@ -113,24 +149,36 @@ class MainView : Fragment("Translator") {
               }
             }
           }
+          prefHeightProperty().bind(rootHeight)
+          minHeightProperty().bind(rootHeight.divide(2))
         }
+        scrollpane {
+          imageview {
+            imageProperty().bind(originalImageSwing)
+            isPreserveRatio = true
+          }
+          originalImageViewPane = this
+        }
+      }
+      vbox {
         textarea(resultText) {
           disableProperty().bind(actionInProgress)
           isEditable = false
+          prefHeightProperty().bind(rootHeight)
+          minHeightProperty().bind(rootHeight.divide(2))
         }
-        orientation = Orientation.HORIZONTAL
-        setDividerPositions(0.5)
-        prefHeightProperty().bind(rootHeight)
-      }
-      splitpane {
-        hbox {
-          imageview { imageProperty().bind(originalImageSwing) }
+        scrollpane {
+          imageview {
+            imageProperty().bind(resultImageSwing)
+            isPreserveRatio = true
+          }
+          resultImageViewPane = this
         }
-        prefHeightProperty().bind(rootHeight)
       }
-      prefHeightProperty().bind(rootHeight)
+      orientation = Orientation.HORIZONTAL
+      setDividerPositions(0.5)
     }
-
+    clearImageAction()
     primaryStage.setOnCloseRequest { exitProcess(0) }
   }
 }
